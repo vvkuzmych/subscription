7fea11b5a1ba5a36be0db51c4b5a756f10a01ffd
Volodymy Kuzmych <vkuzmych@cisco.com>
2024-07-28 21:04:28 +0300

    add attachment manual to email and refactor handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/phpdave11/gofpdf"
+	"github.com/phpdave11/gofpdf/contrib/gofpdi"
 	"html/template"
 	"net/http"
 	"strconv"
 	"subscription/data"
+	"time"
 )
 
 var loginUrl = "/login"
@@ -207,16 +211,57 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 		}
 		app.sendEmail(msg)
 	}()
-	// send an email with the invoice attached
 
 	// generate a manual
+	app.Wait.Add(1)
+	go func() {
+		defer app.Wait.Done()
 
-	// send an email with the manual attached
+		pdf := app.generateManual(user, plan)
+		err := pdf.OutputFileAndClose(fmt.Sprintf("./tmp/%d_manual.pdf", user.ID))
+		if err != nil {
+			app.ErrorChan <- err
+			return
+		}
+		msg := Message{
+			To:      user.Email,
+			Subject: "Your manual",
+			Data:    "You user manual is attached",
+			AttachmentMap: map[string]string{
+				"Manual.pdf": fmt.Sprintf("./tmp/%d_manual.pdf", user.ID),
+			},
+		}
+
+		// send an email with the manual attached
+		app.sendEmail(msg)
+
+		// test app error chan
+		app.ErrorChan <- errors.New("Custom errors")
+	}()
 
 	// subscribe the user to an account
 
 	// redirect
+	app.Session.Put(r.Context(), "flash", "Subscribed!")
+	http.Redirect(w, r, "members/plans", http.StatusSeeOther)
+}
 
+func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
+	pdf := gofpdf.New("P", "mm", "Letter", "")
+	pdf.SetMargins(10, 13, 10)
+
+	importer := gofpdi.NewImporter()
+	time.Sleep(5 * time.Second)
+	t := importer.ImportPage(pdf, "./pdf/manual.pdf", 1, "/MediaBox")
+	pdf.AddPage()
+	importer.UseImportedTemplate(pdf, t, 0, 0, 215.9, 0)
+	pdf.SetX(75)
+	pdf.SetY(150)
+	pdf.SetFont("Arial", "", 12)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s %s", u.FirstName, u.LastName), "", "C", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide", u.FirstName), "", "C", false)
+	return pdf
 }
 
 func (app *Config) getInvoice(u data.User, plan *data.Plan) (string, error) {
--- cmd/web/mailer.go
+++ cmd/web/mailer.go
@@ -26,14 +26,15 @@ type Mail struct {
 }
 
 type Message struct {
-	From        string
-	FromName    string
-	To          string
-	Subject     string
-	Attachments []string
-	Data        any
-	DataMap     map[string]any
-	Template    string
+	From          string
+	FromName      string
+	To            string
+	Subject       string
+	Attachments   []string
+	AttachmentMap map[string]string
+	Data          any
+	DataMap       map[string]any
+	Template      string
 }
 
 func (app *Config) listenForMail() {
@@ -61,12 +62,14 @@ func (m *Mail) sendMail(msg Message, errorChan chan error) {
 	if msg.FromName == "" {
 		msg.FromName = m.FromName
 	}
-
-	data := map[string]any{
-		"message": msg.Data,
+	if msg.AttachmentMap == nil {
+		msg.AttachmentMap = make(map[string]string)
 	}
 
-	msg.DataMap = data
+	if len(msg.DataMap) == 0 {
+		msg.DataMap = make(map[string]any)
+	}
+	msg.DataMap["message"] = msg.Data
 
 	// build html mail
 	formattedMessage, err := m.buildHTMLMessage(msg)
@@ -102,6 +105,11 @@ func (m *Mail) sendMail(msg Message, errorChan chan error) {
 	if len(msg.Attachments) > 0 {
 		for _, x := range msg.Attachments {
 			email.AddAttachment(x)
+		}
+	}
+	if len(msg.AttachmentMap) > 0 {
+		for key, value := range msg.AttachmentMap {
+			email.AddAttachment(value, key)
 		}
 	}
 
