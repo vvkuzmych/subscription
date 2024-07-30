7217e7aada64cc8dcce7ae5291ce046b31469144
Volodymyr Kuzmych <vkuzmych@cisco.com>
2024-07-30 10:08:52 +0300

    add set up main test

---
+++ cmd/web/setup_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/gob"
+	"github.com/alexedwards/scs/v2"
+	"log"
+	"net/http"
+	"os"
+	"subscription/data"
+	"sync"
+	"testing"
+	"time"
+)
+
+var testApp Config
+
+func TestMain(m *testing.M) {
+	gob.Register(data.User{})
+
+	// set up session
+	session := scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	testApp = Config{
+		Session:       session,
+		DB:            nil,
+		InfoLog:       log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		ErrorLog:      log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		Wait:          &sync.WaitGroup{},
+		ErrorChan:     make(chan error),
+		ErrorChanDone: make(chan bool),
+	}
+
+	// create dummy email
+	errorChan := make(chan error)
+	mailerChan := make(chan Message, 100)
+	mailerDoneChan := make(chan bool)
+
+	testApp.Mailer = Mail{
+		Wait:       testApp.Wait,
+		ErrorChan:  errorChan,
+		MailerChan: mailerChan,
+		DoneChan:   mailerDoneChan,
+	}
+
+	go func() {
+		select {
+		case <-testApp.Mailer.MailerChan:
+		case <-testApp.Mailer.ErrorChan:
+		case <-testApp.Mailer.DoneChan:
+			return
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case err := <-testApp.ErrorChan:
+				testApp.ErrorLog.Println(err)
+			case <-testApp.ErrorChanDone:
+				return
+			}
+		}
+	}()
+
+	os.Exit(m.Run())
+}
